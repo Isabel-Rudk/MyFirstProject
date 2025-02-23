237e2d1fbd3e8b5e9a362537d44cec0b2f45396e
Bel <Bel@DESKTOP-68L4A3V>
2025-02-22 21:50:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Bem vindo ao meu primeiropprojeto em C#!");
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá,{nome}! Vamos fazer um cálculo simples");
+
+            Console.Write("Digite um número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite um segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = num1 + num2;
+
+            Console.WriteLine($"A soma de {num1} e {num2} é {soma}.");
+
+            Console.WriteLine("Presione qualquer tecla para sair ...");
+            Console.ReadKey();
+        }
+    }
+}
